aef47c3d48b6084239eb13e3e38e27512c4544da
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-02-19 22:19:10 +0300

    Update included:   - Created the airport and reservations tables   - Added foreign key constraints wherever necessary

--- schema.sql
+++ schema.sql
@@ -6,6 +6,15 @@ DROP DATABASE msafiri_airlines;
 
 CREATE DATABASE IF NOT EXISTS msafiri_airlines;
 
+-- Create the airports table now
+
+CREATE TABLE IF NOT EXISTS airport(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    airport_code VARCHAR(5) UNIQUE,
+    airport_name VARCHAR(20),
+    airport_location VARCHAR(20),
+)
+
 -- Create the flight table
 
 CREATE TABLE IF NOT EXISTS flight(
@@ -13,7 +22,8 @@ CREATE TABLE IF NOT EXISTS flight(
     departure DATE,
     arrival DATE,
     available_seats INT,
-    airport_code VARCHAR(5)
+    airport_code VARCHAR(5),
+    CONSTRAINT airport_id_fk FOREIGN KEY(airport_code) REFERENCES airport(airport_code)
 );
 
 -- Create the passanger table. Include a foreigh key as well
@@ -27,5 +37,18 @@ CREATE TABLE IF NOT EXISTS passanger(
     phone_number VARCHAR(20),
     passport_number VARCHAR(20),
     flight_number INT,
-    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id),
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id)
+);
+
+-- Create the reservations table
+
+CREATE TABLE IF NOT EXISTS reservation(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    passanger_id INT,
+    flight_number INT,
+    reservation_datetime DATETIME,
+    seats_assigned INT,
+    booking_status BOOLEAN,
+    CONSTRAINT passanger_id_fk FOREIGN KEY(passanger_id) REFERENCES passanger(id),
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id)
 );
