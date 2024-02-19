b2f97edfcaf8172603fd27d2b3f9950d50439b7f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-02-19 22:30:41 +0300

    Update included:   - Created the payments and tickets table   - Added foreign keys where applicable

--- schema.sql
+++ schema.sql
@@ -52,3 +52,33 @@ CREATE TABLE IF NOT EXISTS reservation(
     CONSTRAINT passanger_id_fk FOREIGN KEY(passanger_id) REFERENCES passanger(id),
     CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id)
 );
+
+-- Create the tickets table
+
+CREATE TABLE IF NOT EXISTS tickets(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    passanger_id INT,
+    flight_id INT,
+    seat_number INT,
+    class VARCHAR(10),
+    ticket_price DECIMAL(10, 2),
+    date_issued DATETIME,
+    airport_code VARCHAR(5),
+    CONSTRAINT passanger_id_fk FOREIGN KEY(passanger_id) REFERENCES passanger(id),
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id),
+    CONSTRAINT airport_id_fk FOREIGN KEY(airport_code) REFERENCES airport(airport_code)
+);
+
+-- Create the payment table now
+
+CREATE TABLE IF NOT EXISTS payment(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    reservation_id INT,
+    amount_paid DECIMAL(10, 2),
+    payment_date DATETIME,
+    payment_method VARCHAR(30),
+    transaction_status BOOLEAN,
+    flight_id INT,
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id),
+    CONSTRAINT reservation_id_fk FOREIGN KEY(reservation_id) REFERENCES reservation(id),
+);
