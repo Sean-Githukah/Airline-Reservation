0a9db5aa97f10d579c4aee1449833ed0ec55912c
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-02-19 22:56:44 +0300

    Update included:   - Fixed some found typos   - Updated project documentation

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Airline-Reservation
+# Msafiri Airlines
+
+# Guide on how to test the database locally
+
+1. Navigate to the folder/ directory were you want the code project to be in
+
+2. Clone this repository using
+
+```git clone git@github.com:Sean-Githukah/Airline-Reservation.git```
+
+3. cd or navigate into the folder that will be creates when you clone this repository
+
+```cd Airline-Reservation```
+
+4. Open the project using a text editor of your choice. I recommend using visual studio code for this. Incase you do not have visual studio code visit [this website](https://code.visualstudio.com/download) to download and install visual studio code.
+
+
+If you have visual studio code, you can open it by running ```code .``` and hitting enter.
+
+
+Optionally open visual studio code and select "file" then "open" then navigate to where the project code was stored. This will also work well.
+
+
+# We now need to create the database locally for this we need to first access our PostgreSQL database. Follow the following steps:
+
+1. In your command prompt or terminal type the following then hit enter ```psql -U postgres```
+
+2. You will then be ablt to type in the commands that are in the schema.sql file and hit enter.
+
+
+# Instead of going through all this, you can use the PGAdmin4 graphical user interface application. It will help you avoid using the terminal/ command prompt.
+
+[This video](https://youtu.be/UjQiwonRMas?si=Uev1gm7E8FcdXhCr) and this one will help you know how to use the PGAdmin4 aplication [this video](https://youtu.be/WFT5MaZN6g4?si=Qlhozbnb8d9L6bPG) as well is great.
+
+
+Incase you do not have PGAdmin4 in your laptop, please search how to isntall it for your operating system
--- schema.sql
+++ schema.sql
@@ -1,23 +1,19 @@
 -- We shall drop the database incase there is a similar db
-
-DROP DATABASE msafiri_airlines;
+DROP DATABASE IF EXISTS msafiri_airlines;
 
 -- Create our database
-
 CREATE DATABASE IF NOT EXISTS msafiri_airlines;
 
 -- Create the airports table now
-
-CREATE TABLE IF NOT EXISTS airport(
+CREATE TABLE IF NOT EXISTS airport (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     airport_code VARCHAR(5) UNIQUE,
-    airport_name VARCHAR(20),
-    airport_location VARCHAR(20),
-)
+    airport_name VARCHAR(50),
+    airport_location VARCHAR(50)
+);
 
 -- Create the flight table
-
-CREATE TABLE IF NOT EXISTS flight(
+CREATE TABLE IF NOT EXISTS flight (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     departure DATE,
     arrival DATE,
@@ -26,59 +22,51 @@ CREATE TABLE IF NOT EXISTS flight(
     CONSTRAINT airport_id_fk FOREIGN KEY(airport_code) REFERENCES airport(airport_code)
 );
 
--- Create the passanger table. Include a foreigh key as well
-
-CREATE TABLE IF NOT EXISTS passanger(
+-- Create the passenger table. Include a foreign key as well
+CREATE TABLE IF NOT EXISTS passenger (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     date_of_birth DATE,
-    gender VARCHAR(20),
+    gender VARCHAR(10),
     phone_number VARCHAR(20),
     passport_number VARCHAR(20),
-    flight_number INT,
-    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id)
+    flight_id INT,
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_id) REFERENCES flight(id)
 );
 
 -- Create the reservations table
-
-CREATE TABLE IF NOT EXISTS reservation(
+CREATE TABLE IF NOT EXISTS reservation (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    passanger_id INT,
-    flight_number INT,
-    reservation_datetime DATETIME,
+    passenger_id INT,
+    flight_id INT,
+    reservation_datetime TIMESTAMP,
     seats_assigned INT,
     booking_status BOOLEAN,
-    CONSTRAINT passanger_id_fk FOREIGN KEY(passanger_id) REFERENCES passanger(id),
-    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id)
+    CONSTRAINT passenger_id_fk FOREIGN KEY(passenger_id) REFERENCES passenger(id),
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_id) REFERENCES flight(id)
 );
 
 -- Create the tickets table
-
-CREATE TABLE IF NOT EXISTS tickets(
+CREATE TABLE IF NOT EXISTS tickets (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    passanger_id INT,
+    passenger_id INT,
     flight_id INT,
     seat_number INT,
     class VARCHAR(10),
     ticket_price DECIMAL(10, 2),
-    date_issued DATETIME,
-    airport_code VARCHAR(5),
-    CONSTRAINT passanger_id_fk FOREIGN KEY(passanger_id) REFERENCES passanger(id),
-    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id),
-    CONSTRAINT airport_id_fk FOREIGN KEY(airport_code) REFERENCES airport(airport_code)
+    date_issued TIMESTAMP,
+    CONSTRAINT passenger_id_fk FOREIGN KEY(passenger_id) REFERENCES passenger(id),
+    CONSTRAINT flight_id_fk FOREIGN KEY(flight_id) REFERENCES flight(id)
 );
 
 -- Create the payment table now
-
-CREATE TABLE IF NOT EXISTS payment(
+CREATE TABLE IF NOT EXISTS payment (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     reservation_id INT,
     amount_paid DECIMAL(10, 2),
-    payment_date DATETIME,
+    payment_date TIMESTAMP,
     payment_method VARCHAR(30),
     transaction_status BOOLEAN,
-    flight_id INT,
-    CONSTRAINT flight_id_fk FOREIGN KEY(flight_number) REFERENCES flight(id),
-    CONSTRAINT reservation_id_fk FOREIGN KEY(reservation_id) REFERENCES reservation(id),
+    CONSTRAINT reservation_id_fk FOREIGN KEY(reservation_id) REFERENCES reservation(id)
 );
